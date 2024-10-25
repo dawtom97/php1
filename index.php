c0d5f8999bde58e051f740f1e1fc2913bece189e
dawtom97 <daw.tomczyk1997@gmail.com>
2024-10-25 10:23:17 +0200

    mig11

--- index.php
+++ index.php
@@ -10,6 +10,11 @@ use App\Migrations\DatabaseMigration;
 
 $env = getenv('APP_MODE') ?: 'dev'; // Domyślnie ustaw na 'development'
 echo $env;
+var_dump(get_include_path()); // Sprawdź, jakie są dostępne ścieżki
+var_dump(class_exists('App\Migrations\DatabaseMigration')); // Sprawdź, czy klasa istnieje
+
+
+
 if ($env === 'prod') {
     $config = require 'config.production.php';
     $migration = new DatabaseMigration();
